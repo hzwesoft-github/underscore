ed034444583b13f0c5eb256c6d69f33b2c0c62d4
zddava <zddava@gmail.com>
2022-10-29 09:50:56 +0800

    m

--- lang/lang.go
+++ lang/lang.go
@@ -1,6 +1,7 @@
 package lang
 
 import (
+	"strconv"
 	"strings"
 )
 
@@ -14,4 +15,30 @@ func TernaryOperator[T any](cond bool, value1 T, value2 T) T {
 	}
 
 	return value2
+}
+
+func StrToInt[T int | int8 | int16 | int32 | int64](str string, base, bitSize int, defaultVal T) T {
+	ret, err := strconv.ParseInt(str, base, bitSize)
+	if err != nil {
+		return defaultVal
+	}
+
+	return T(ret)
+}
+
+func StrToUint[T uint | uint8 | uint16 | uint32 | uint64](str string, base, bitSize int, defaultVal T) T {
+	ret, err := strconv.ParseUint(str, base, bitSize)
+	if err != nil {
+		return defaultVal
+	}
+
+	return T(ret)
+}
+
+func StrToBool(str string, defaultVal bool) bool {
+	ret, err := strconv.ParseBool(str)
+	if err != nil {
+		return defaultVal
+	}
+	return ret
 }
